a99f91baf057c947c2b52a5aad7c679a831aac6f
hirano akira <a>
2025-02-09 12:01:07 +0900

    second

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-import streamlit as st
+'''
+import streamlit as st 
+import random
+if st.button("label"):
+    random.number = random.randint(1,100)
+'''    
+
+import streamlit as st 
+import random
+if st.button("label"):
+    number = random.randint(1,1000)
+    st.write(number)
